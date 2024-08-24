5775f5cb2858879860435b236a2dd7b8428c581c
PedroItaloQ <peedroitalo2906@gmail.com>
2024-08-24 18:28:50 -0300

    feat: updated in UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,6 +5,29 @@ import bcrypt from 'bcrypt';
 import jwt from "jsonwebtoken";
 
 export class UserController{
+    async create(req: Request, res: Response) {
+        const { username, email, password } = req.body;
+
+        const existingUser = await userRepository.findOneBy({ email });
+
+        if (existingUser) {
+            throw new BadRequestError("Usuário já cadastrado!");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = userRepository.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        await userRepository.save(newUser);
+
+        return res.status(201).json({ message: "Usuário criado com sucesso!" });
+    }
+
+
     async login(req: Request, res: Response) {
         const { email, password } = req.body;
 
@@ -22,6 +45,6 @@ export class UserController{
 
         const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
 
-        return res.json({ token });
+        return res.json({ token, email: user.email, username: user.username });
     }
 }
