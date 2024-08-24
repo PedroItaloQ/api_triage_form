06e50308f1a59cb3fe06923638bd5836a97dae9f
PedroItaloQ <peedroitalo2906@gmail.com>
2024-08-24 18:30:16 -0300

    feat: Created User entitie

---
+++ src/entities/User.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+        password: string;
+}
