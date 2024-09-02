b92c139098f6eee370dc8a07a4f4ceef75257d3d
PedroItaloQ <peedroitalo2906@gmail.com>
2024-09-02 12:55:06 -0300

    feat: added error

--- src/controllers/TriageController.ts
+++ src/controllers/TriageController.ts
@@ -1,141 +1,149 @@
 import { Request, Response } from "express";
 import { triageRepository } from "../repositories/triageRepository";
-// import { BadRequestError } from "../helpers/api-erros";
+import { BadRequestError } from "../helpers/api-erros"; // Certifique-se de que este import está correto
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import 'dotenv/config';
-import { BadRequestError } from "../helpers/api-erros";
 
 export class TriageController {
     async create(req: Request, res: Response) {
-        const {
-            numberOfProcess,
-            author,
-            cpf,
-            bccReceiptDate,
-            bccReceiptTime,
-            captureDate,
-            captureTime,
-            distributionData,
-            processSystem,
-            typeOfCommunication,
-            communicationDate,
-            communicationTime,
-            endDateOfCommunication,
-            reu,
-            classe,
-            foro,
-            internalCode,
-            vara,
-            comarca,
-            justiceSecret,
-            tribunalDeOrigem,
-            subject,
-            hearingDate,
-            hearingTime,
-            causeValue,
-            forFulfillment,
-            fine,
-            tipeOfFine,
-            valueOfFine,
-            fatalDeadline,
-            assigned,
-            obfDescription,
-            observation,
-        } = req.body;
+        try {
+            const {
+                numberOfProcess,
+                author,
+                cpf,
+                bccReceiptDate,
+                bccReceiptTime,
+                captureDate,
+                captureTime,
+                distributionData,
+                processSystem,
+                typeOfCommunication,
+                communicationDate,
+                communicationTime,
+                endDateOfCommunication,
+                reu,
+                classe,
+                foro,
+                internalCode,
+                vara,
+                comarca,
+                justiceSecret,
+                tribunalDeOrigem,
+                subject,
+                hearingDate,
+                hearingTime,
+                causeValue,
+                forFulfillment,
+                fine,
+                tipeOfFine,
+                valueOfFine,
+                fatalDeadline,
+                assigned,
+                obfDescription,
+                observation,
+            } = req.body;
 
-        console.log(
-            numberOfProcess,
-            author,
-            cpf,
-            bccReceiptDate,
-            bccReceiptTime,
-            captureDate,
-            captureTime,
-            distributionData,
-            processSystem,
-            typeOfCommunication,
-            communicationDate,
-            communicationTime,
-            endDateOfCommunication,
-            reu,
-            classe,
-            foro,
-            internalCode,
-            vara,
-            comarca,
-            justiceSecret,
-            tribunalDeOrigem,
-            subject,
-            hearingDate,
-            hearingTime,
-            causeValue,
-            forFulfillment,
-            fine,
-            tipeOfFine,
-            valueOfFine,
-            fatalDeadline,
-            assigned,
-            obfDescription,
-            observation,
-        );
+            console.log(
+                numberOfProcess,
+                author,
+                cpf,
+                bccReceiptDate,
+                bccReceiptTime,
+                captureDate,
+                captureTime,
+                distributionData,
+                processSystem,
+                typeOfCommunication,
+                communicationDate,
+                communicationTime,
+                endDateOfCommunication,
+                reu,
+                classe,
+                foro,
+                internalCode,
+                vara,
+                comarca,
+                justiceSecret,
+                tribunalDeOrigem,
+                subject,
+                hearingDate,
+                hearingTime,
+                causeValue,
+                forFulfillment,
+                fine,
+                tipeOfFine,
+                valueOfFine,
+                fatalDeadline,
+                assigned,
+                obfDescription,
+                observation,
+            );
 
-        const processExists = await triageRepository.findOneBy({ numberOfProcess });
+            const processExists = await triageRepository.findOneBy({ numberOfProcess });
 
-        if (processExists) {
-            throw new BadRequestError("Número de processo já existe!");
-        }
+            if (processExists) {
+                throw new BadRequestError("Número de processo já existe!");
+            }
 
-        const newTriage = triageRepository.create({ 
-            numberOfProcess,
-            author,
-            cpf,
-            bccReceiptDate,
-            bccReceiptTime,
-            captureDate,
-            captureTime,
-            distributionData,
-            processSystem,
-            typeOfCommunication,
-            communicationDate,
-            communicationTime,
-            endDateOfCommunication,
-            reu,
-            classe,
-            foro,
-            internalCode,
-            vara,
-            comarca,
-            justiceSecret,
-            tribunalDeOrigem,
-            subject,
-            hearingDate,
-            hearingTime,
-            causeValue,
-            forFulfillment,
-            fine,
-            tipeOfFine,
-            valueOfFine,
-            fatalDeadline,
-            assigned,
-            obfDescription,
-            observation,
-        });
+            const newTriage = triageRepository.create({
+                numberOfProcess,
+                author,
+                cpf,
+                bccReceiptDate,
+                bccReceiptTime,
+                captureDate,
+                captureTime,
+                distributionData,
+                processSystem,
+                typeOfCommunication,
+                communicationDate,
+                communicationTime,
+                endDateOfCommunication,
+                reu,
+                classe,
+                foro,
+                internalCode,
+                vara,
+                comarca,
+                justiceSecret,
+                tribunalDeOrigem,
+                subject,
+                hearingDate,
+                hearingTime,
+                causeValue,
+                forFulfillment,
+                fine,
+                tipeOfFine,
+                valueOfFine,
+                fatalDeadline,
+                assigned,
+                obfDescription,
+                observation,
+            });
 
-        await triageRepository.save(newTriage);
+            await triageRepository.save(newTriage);
 
-        const { ...triage } = newTriage
+            const { ...triage } = newTriage
 
-        return res.status(201).json(triage);
-    };
+            return res.status(201).json(triage);
 
-    async getAll(req: Request, res: Response) {
-        try{
-            const triages = await triageRepository.find();
-            return res.json(triages);
-        }catch (error) {
-            throw new BadRequestError("Erro ao buscar as triagens.")
+        } catch (error) {
+            if (error instanceof BadRequestError) {
+                return res.status(400).json({ error: error.message });
+            } else {
+                console.error(error);
+                return res.status(500).json({ error: "Erro interno do servidor." });
+            }
         }
     }
 
-};
+    async getAll(req: Request, res: Response) {
+        try {
+            const triages = await triageRepository.find();
+            return res.json(triages);
+        } catch (error) {
+            return res.status(400).json({ error: "Erro ao buscar as triagens." });
+        }
+    }
+}
