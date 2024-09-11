0a692fbb59ea8d347dd8345e158e7a38fa1ada5c
PedroItaloQ <peedroitalo2906@gmail.com>
2024-09-11 11:09:40 -0300

    refat: Updated in CreateTriage nullable

--- src/entities/CreateTriage.ts
+++ src/entities/CreateTriage.ts
@@ -3,110 +3,110 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 @Entity('triage')
 export class Triage {
 	@PrimaryGeneratedColumn()
-	id: number
+  id: number;
 
-	@Column()
-	numberOfProcess: string;
+  @Column()
+  numberOfProcess: string;
 
-	@Column()
-	author: string;
+  @Column()
+  author: string;
 
-	@Column()
-	cpf: string;
+  @Column()
+  cpf: string;
 
-	@Column()
-	bccReceiptDate: string;
+  @Column({ nullable: true })
+  bccReceiptDate: string;
 
-	@Column()
-	bccReceiptTime: string;
+  @Column({ nullable: true })
+  bccReceiptTime: string;
 
-	@Column()
-	captureDate: string;
+  @Column({ nullable: true })
+  captureDate: string;
 
-	@Column()
-	captureTime: string;
-	
-	@Column()
-	distributionData: string;
-	
-	@Column()
-	processSystem: string;
-	
-	@Column()
-	typeOfCommunication: string;
-	
-	@Column()
-	communicationDate: string;
-	
-	@Column()
-	communicationTime: string;
-	
-	@Column()
-	endDateOfCommunication: string;
-	
-	@Column()
-	reu: string;
-	
-	@Column()
-	classe: string;
-	
-	@Column()
-	foro: string;
-	
-	@Column()
-	internalCode: string;
-	
-	@Column()
-	vara: string;
-	
-	@Column()
-	comarca: string;
-	
-	@Column()
-	justiceSecret: string;
-	
-	@Column()
-	tribunalDeOrigem: string;
-	
-	@Column()
-	subject: string;
-	
-	@Column()
-	hearingDate: string;
-	
-	@Column()
-	hearingTime: string;
-	
-	@Column()
-	causeValue: string;
-	
-	@Column()
-	forFulfillment: string;
-	
-	@Column()
-	fine: string;
-	
-	@Column()
-	tipeOfFine: string;
-	
-	@Column()
-	valueOfFine: string;
-	
-	@Column()
-	fatalDeadline: string;
-	
-	@Column()
-	assigned: string;
-	
-	@Column()
-	obfDescription: string;
+  @Column({ nullable: true })
+  captureTime: string;
 
-	@Column()
-	status: boolean;
+  @Column({ nullable: true })
+  distributionData: string;
 
-	@Column()
-	state: string;
-	
-	@Column()
-	observation: string;
+  @Column({ nullable: true })
+  processSystem: string;
+
+  @Column({ nullable: true })
+  typeOfCommunication: string;
+
+  @Column({ nullable: true })
+  communicationDate: string;
+
+  @Column({ nullable: true })
+  communicationTime: string;
+
+  @Column({ nullable: true })
+  endDateOfCommunication: string;
+
+  @Column({ nullable: true })
+  reu: string;
+
+  @Column({ nullable: true })
+  classe: string;
+
+  @Column({ nullable: true })
+  foro: string;
+
+  @Column({ nullable: true })
+  internalCode: string;
+
+  @Column({ nullable: true })
+  vara: string;
+
+  @Column({ nullable: true })
+  comarca: string;
+
+  @Column({ nullable: true })
+  justiceSecret: string;
+
+  @Column({ nullable: true })
+  tribunalDeOrigem: string;
+
+  @Column({ nullable: true })
+  subject: string;
+
+  @Column({ nullable: true })
+  hearingDate: string;
+
+  @Column({ nullable: true })
+  hearingTime: string;
+
+  @Column({ nullable: true })
+  causeValue: string;
+
+  @Column({ nullable: true })
+  forFulfillment: string;
+
+  @Column({ nullable: true })
+  fine: string;
+
+  @Column({ nullable: true })
+  tipeOfFine: string;
+
+  @Column({ nullable: true })
+  valueOfFine: string;
+
+  @Column({ nullable: true })
+  fatalDeadline: string;
+
+  @Column({ nullable: true })
+  assigned: string;
+
+  @Column({ nullable: true })
+  obfDescription: string;
+
+  @Column({ nullable: true })
+  status: boolean;
+
+  @Column({ nullable: true })
+  state: string;
+
+  @Column({ nullable: true })
+  observation: string;
 }
