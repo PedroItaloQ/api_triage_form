48524e901cbd41c9c4ef471963c640c6f6e19945
PedroItaloQ <peedroitalo2906@gmail.com>
2024-09-11 11:08:12 -0300

    feat: added routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,16 +3,21 @@ import { TriageController } from '../src/controllers/TriageController';
 import { UserController } from '../src/controllers/UserController';
 import { authMiddleware } from './middlewares/authMiddleware';
 import multer from 'multer';
+import { ImportedTriageController } from './controllers/ImportedTriageController';
 
 const routes = Router();
 
 const upload = multer({ dest: 'uploads/' });
+const importedTriageController = new ImportedTriageController();
 
 const triageController = new TriageController();
 const userController = new UserController();
 
 routes.post("/triagem", authMiddleware, triageController.create);
 routes.get("/triagem", triageController.getAll);
+
+routes.post("/imported-triage/importar-linhas", importedTriageController.importRowsToTriage);
+routes.get("/triagem-importados", importedTriageController.getAll);
 
 routes.post("/auth/login", userController.login);
 
