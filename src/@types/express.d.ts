8f33b45937a32dad75bbadd46c8c3f3f35d7b262
PedroItaloQ <peedroitalo2906@gmail.com>
2024-10-03 09:33:45 -0300

    feat: Added triageAdm in types

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,4 +1,5 @@
 import { Triage } from '../entities/CreateTriage';
+import { TriageAdm } from '../entities/CreateTriageAdm';
 import { User } from '../entities/User';
 
 declare global {
@@ -6,6 +7,7 @@ declare global {
 		export interface Request {
 			triage: Partial<Triage>;
 			user: Partial<User>;
+			triageAdm: Partial<TriageAdm>
 		}
 	}
 }
