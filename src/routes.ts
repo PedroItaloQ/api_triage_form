fd8ec92246e67fa3cb165a0b8dddfeafc443f9e1
PedroItaloQ <peedroitalo2906@gmail.com>
2024-10-03 09:33:22 -0300

    feat: Added routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { TriageController } from '../src/controllers/TriageController';
+import { TriageAdmController } from './controllers/TriageAdmController';
 import { UserController } from '../src/controllers/UserController';
 import { authMiddleware } from './middlewares/authMiddleware';
 import multer from 'multer';
@@ -11,11 +12,16 @@ const upload = multer({ dest: 'uploads/' });
 const importedTriageController = new ImportedTriageController();
 
 const triageController = new TriageController();
+const triagesAdmController = new TriageAdmController();
 const userController = new UserController();
 
 routes.post("/triagem", authMiddleware, triageController.create);
 routes.get("/triagem", triageController.getAll);
 routes.put("/triagem/:numberOfProcess", authMiddleware, triageController.update);
+
+routes.post("/triagem-admin", authMiddleware, triagesAdmController.create);
+routes.get("/triagem-admin" , authMiddleware, triagesAdmController.getAll);
+
 
 routes.post("/imported-triage/importar-linhas", importedTriageController.importRowsToTriage);
 routes.get("/triagem-importados", importedTriageController.getAll);
@@ -23,7 +29,7 @@ routes.get("/triagem-importados", importedTriageController.getAll);
 routes.post("/auth/login", userController.login);
 
 routes.post("/register", userController.create);
-routes.get("/users", userController.getAllUsers)
+routes.get("/users", userController.getAllUsers);
 routes.put("/users/:id", authMiddleware, userController.update);
 
 routes.post("/upload", upload.single('file'), userController.uploadXlsx);
